62399b4d620396778388f2083d5c4da3f7e7bbd9
Christian <christian@acivilate.com>
2023-05-11 21:15:18 -0500

    Made measurements more precise, and responses better

--- searchFoodTrucks.js
+++ searchFoodTrucks.js
@@ -4,20 +4,21 @@ const geolib = require('geolib');
 const nodeGeoCoder = require('node-geocoder');
 
 const options = {
-    provider: 'openstreetmap',
-    // provider: 'google',
-    // apiKey: 'YOUR_API_KEY',
+    // provider: 'openstreetmap',
+     provider: 'google',
+     apiKey: '',
     formatter: null
 }
+
+const defaultRadius = 100; // 1/16 mile in meters
 
 async function geoLocation(query) {
     let resultLocations;
 
     const geocoder = nodeGeoCoder(options);
-
-    const radius = geolib.convertDistance(50, 'mi')*1000;
     resultLocations = await geocoder.geocode(query);
-    return {resultLocations, radius};
+    console.log('RESULT LOCATIONS: ', await resultLocations)
+    return resultLocations;
 }
 
 function filterByTruckName(query, locationData) {
@@ -28,33 +29,46 @@ function filterByTruckName(query, locationData) {
     );
 }
 
-function isLocationWithinRadius(row, lat, lon, radius) {
-    return geolib.isPointWithinRadius(
+function precisionDistance(start, end, accuracy = 5) {
+    const distance = geolib.getPreciseDistance(start, end, accuracy);
+    // The distance will be in meters, you can convert it to miles if needed
+    return geolib.convertDistance(distance, 'mi');
+}
+
+function isLocationWithinRadius(row, lat, lon, radiusMeters) {
+   return geolib.isPointWithinRadius(
         {latitude: parseFloat(row.Latitude), longitude: parseFloat(row.Longitude)},
         {latitude: lat, longitude: lon},
-        radius
+        radiusMeters // 1/16 mile
     );
 }
 
-function buildFinalResult(filteredData) {
-    return filteredData.map(row => ({
-        locationId: row.locationid,
-        applicant: row.Applicant,
-        address: row.Address,
-        locationDescription: row.LocationDescription,
-        blocklot: row.blocklot,
-        permit: row.permit,
-        status: row.Status,
-        approved: row.Approved,
-        schedule: row.Schedule,
-        daysHours: row.dayshours || 'Hourly schedule not posted',
-        foodItems: row.FoodItems
-    }));
+function buildFinalResult(filteredData, locationsResult) {
+    return filteredData.map(row => {
+        const distanceInMiles = locationsResult ? geolib.getPreciseDistance(
+            {latitude: row.Latitude, longitude: row.Longitude},
+            {latitude: locationsResult.latitude, longitude: locationsResult.longitude}
+        )/ 1609.34 : ''
+
+        return {
+            locationId: row.locationid,
+            applicant: row.Applicant,
+            address: row.Address,
+            locationDescription: row.LocationDescription,
+            blocklot: row.blocklot,
+            permit: row.permit,
+            status: row.Status,
+            approved: row.Approved,
+            schedule: row.Schedule,
+            daysHours: row.dayshours || 'Hourly schedule not posted',
+            foodItems: row.FoodItems,
+            distance: locationsResult ? Number(distanceInMiles.toFixed(2)) + ` miles from ${row.Address}`  : null
+        }});
 }
 
 async function searchFoodTrucks(query, callback) {
     let  data = [];
-    let filteredData = [];
+    let filteredLocations = [];
 
     fs.createReadStream('mobile_food_facility_permit.csv')
         .pipe(csv())
@@ -63,18 +77,18 @@ async function searchFoodTrucks(query, callback) {
         })
         .on('end', async () => {
             if (/[a-zA-Z]/.test(query)) {
-                filteredData = filterByTruckName(query, data);
+                filteredLocations = filterByTruckName(query, data);
             }
 
-            const {resultLocations, radius} = await geoLocation(query);
+            const resultLocations = await geoLocation(query);
             if (resultLocations.length > 0) {
                 const lat = resultLocations[0].latitude;
                 const lon = resultLocations[0].longitude;
 
-                filteredData = [...filteredData, ...data.filter(row => isLocationWithinRadius(row, lat, lon, radius))]
+                filteredLocations = [...filteredLocations, ...data.filter(row => isLocationWithinRadius(row, lat, lon, defaultRadius))]
             }
 
-            callback(null, buildFinalResult(filteredData));
+            callback(null, buildFinalResult(filteredLocations, resultLocations.length > 0 ? resultLocations[0] : null));
         });
 }
 exports.searchFoodTrucks = searchFoodTrucks;
