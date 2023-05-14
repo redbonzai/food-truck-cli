5a680df867c90bd8a43b375d9bf68cb787b7ac87
Christian <christian@acivilate.com>
2023-05-14 15:19:06 -0500

    following convention to single responsibility methods, and testing all scenarios to ensure proper function.  Update documentation

--- index.js
+++ index.js
@@ -1,20 +1,64 @@
 #!/usr/bin/env node
 
-const {searchFoodTrucks} = require('./searchFoodTrucks');
+const yargs = require('yargs');
+const {
+    searchFoodTrucks,
+    isZipCode,
+    isAddress,
+} = require('./searchFoodTrucks');
 
-const query = process.argv[2];
+const argv = yargs
+    .option('address', {
+        alias: 'a',
+        describe: 'Address or zip code for filtering',
+        type: 'string'
+    })
+    .option('radius', {
+        alias: 'r',
+        describe: 'Radius in meters',
+        type: 'number'
+    })
+    .option('name', {
+        alias: 'n',
+        describe: 'string that is contained in a column value - food truck name, food item, etc.',
+        type: 'string'
+    })
+    .demandCommand(1, 'Please provide a search query: Address, Food Truck Name, Food Item')
+    .strict()
+    .check((argv) => {
+        if (typeof argv.radius === 'number' || argv.radius === undefined) {
+            return true;
+        } else {
+            throw new Error('Error: The --radius value must be a number.');
+        }
+    })
+    .parse();
 
-if (!query) {
-    console.error('Please provide a search query: Address, Food Truck Name, Food Item');
+const query = argv._[0];
+const radius = argv.radius;
+const address = argv.address;
+const name = argv.name;
+
+if ((!isZipCode(query) && !isAddress(query)) && radius && !address) {
+    console.error(`When using a non-address query like: "${query}", you must provide an address to geo-locate on as well.`);
     process.exit(1);
 }
+const responseMessage = (results) => {
+    return isAddress(query) || isZipCode(query)
+        ? `Found ${results.length} food trucks within a ${Number(radius / 1609.344).toFixed(2)} mile range of ${query} `
+        : `Found ${results.length} food trucks within the search results for: ${query}`;
+}
 
-searchFoodTrucks(query, (error, results) => {
-    if (error) {
-        console.error('THERE HAS BEEN AN ERROR: ', error);
-        process.exit(1);
-    } else {
-        console.log(`Found ${results.length} food trucks within range of ${query}:`, results);
+const run = async () => {
+    try {
+        const results = await searchFoodTrucks(query, radius, name, address);
+        console.log(results);
+        console.log(responseMessage(results));
         process.exit(0);
+    } catch (error) {
+        console.error('THERE HAS BEEN AN ERROR:', error);
+        process.exit(1);
     }
-});
+}
+
+run();
--- searchFoodTrucks.js
+++ searchFoodTrucks.js
@@ -5,41 +5,74 @@ const nodeGeoCoder = require('node-geocoder');
 
 const options = {
     provider: 'openstreetmap',
-    //  provider: 'google',
-     apiKey: '',
+    // provider: 'google',
+    // apiKey: '',
     formatter: null
 }
 
-const defaultRadius = 100; // 1/16 mile in meters
+const isZipCode = (query) => {
+    const zipCodeRegex = /^\d{5}$/;
+    return zipCodeRegex.test(query);
+}
 
-async function geoLocation(query) {
+const isAddress = (query) => {
+    // Regex pattern to match address format (e.g., 123 Main St, City, State)
+    const addressRegex = /(\d+\s+\w+\s+\w+)|(\d+\s+\w+)/;
+    return addressRegex.test(query);
+}
+
+const getFirstResultOrDefault = (results) =>
+    results.length > 0 ? results[0] : null;
+
+const csvData = (filePath) => {
+    return new Promise((resolve, reject) => {
+        const data = [];
+
+        fs.createReadStream(filePath)
+            .pipe(csv())
+            .on('data', row => data.push(row))
+            .on('end', () => resolve(data))
+            .on('error', error => reject(error));
+    });
+}
+
+const geoLocation = async (query) => {
     const geocoder = nodeGeoCoder(options);
     return await geocoder.geocode(query);
 }
 
-function filterByTruckName(query, locationData) {
-    const applicant = query.toLowerCase().trim();
+const mergeGeoResultsWithCsv = (csvData, geoLocatedResults, defaultRadius) => {
+    return csvData.filter((csvRow) => {
+        return geoLocatedResults.some((geoResult) => {
+            const {latitude, longitude} = geoResult;
+            return isLocationWithinRadius(csvRow, latitude, longitude, defaultRadius);
+        });
+    });
+}
 
-    return locationData.filter(row => Object.values(row)
-        .some(val => typeof val === 'string' && val.toLowerCase().includes(applicant))
+const filterByColumnValues = (query, csvData) => {
+    console.log('query', query)
+    const search = query.toLowerCase().trim();
+
+    return csvData.filter(row => Object.values(row)
+        .some(val => typeof val === 'string' && val.toLowerCase().includes(search))
     );
 }
 
-function precisionDistance(start, end, accuracy = 5) {
-    const distance = geolib.getPreciseDistance(start, end, accuracy);
-    // The distance will be in meters, you can convert it to miles if needed
-    return geolib.convertDistance(distance, 'mi');
+const precisionDistance = (start, end, accuracy = 1) => {
+    const distanceInMeters = geolib.getPreciseDistance(start, end, accuracy);
+    return geolib.convertDistance(distanceInMeters, 'mi');
 }
 
-function isLocationWithinRadius(row, lat, lon, radiusMeters) {
+const isLocationWithinRadius = (locatedRow, lat, lon, radiusMeters) => {
    return geolib.isPointWithinRadius(
-        {latitude: parseFloat(row.Latitude), longitude: parseFloat(row.Longitude)},
+        {latitude: parseFloat(locatedRow.Latitude), longitude: parseFloat(locatedRow.Longitude)},
         {latitude: lat, longitude: lon},
-        radiusMeters // 1/16 mile
+        radiusMeters
     );
 }
 
-function buildFinalResult(filteredData, locationsResult) {
+const buildFinalResult = (filteredData, locationsResult) => {
     return filteredData.map(row => {
         const {
             Latitude,
@@ -58,14 +91,10 @@ function buildFinalResult(filteredData, locationsResult) {
         } = row;
 
         const distanceInMiles = locationsResult
-            ? geolib.getPreciseDistance(
+            ? precisionDistance(
             { latitude: Latitude, longitude: Longitude },
-            {
-                latitude: locationsResult.latitude,
-                longitude: locationsResult.longitude
-            }
-        ) / 1609.34
-            : '';
+            { latitude: locationsResult.latitude, longitude: locationsResult.longitude }
+        ) : '';
 
         return {
             locationId: locationid,
@@ -79,40 +108,78 @@ function buildFinalResult(filteredData, locationsResult) {
             schedule: Schedule,
             daysHours: dayshours || 'Hourly schedule not posted',
             foodItems: FoodItems,
-            distance: locationsResult ? Number(distanceInMiles.toFixed(2)) + ` miles from ${Address}` : null
+            distance: locationsResult ?  Number(distanceInMiles).toFixed(4) + ` miles from ${Address}` : null
         };
     });
 }
 
+async function searchFoodTrucks(query, radius, name, address) {
+    try {
+        const {geoLocatedResults , filteredLocations}
+            = await compileSearch(query, radius, address, name)
 
-async function searchFoodTrucks(query, callback) {
-    let  data = [];
-    let filteredLocations = [];
+        return buildFinalResult(
+            filteredLocations,
+            getFirstResultOrDefault(geoLocatedResults)
+        );
 
-    fs.createReadStream('mobile_food_facility_permit.csv')
-        .pipe(csv())
-        .on('data', row => {
-            data.push(row);
-        })
-        .on('end', async () => {
-            if (/[a-zA-Z]/.test(query)) {
-                filteredLocations = filterByTruckName(query, data);
-            }
-
-            const resultLocations = await geoLocation(query);
-            if (resultLocations.length > 0) {
-                const lat = resultLocations[0].latitude;
-                const lon = resultLocations[0].longitude;
-
-                filteredLocations = [...filteredLocations, ...data.filter(row => isLocationWithinRadius(row, lat, lon, defaultRadius))]
-            }
-
-            const getFirstResultOrDefault = (results) =>
-                results.length > 0
-                    ? results[0]
-                    : null;
-
-            callback(null, buildFinalResult(filteredLocations, getFirstResultOrDefault(resultLocations)));
-        });
+    } catch (error) {
+        console.log('There was an error searching for food trucks:', error)
+        throw new Error(error)
+    }
 }
-exports.searchFoodTrucks = searchFoodTrucks;
+
+const compileSearch = async(query, radius, address, name) => {
+    const defaultRadius = radius || 200; // 1/12 mile in meters
+    let geoLocatedResults = [];
+    let filteredLocations = [];
+    const data = await csvData('mobile_food_facility_permit.csv');
+
+    // case 4: non address query with optional radius, and optional address
+    // geolocate the optional address then filter through csv for lat / long points within default radius distance of address
+    // then look for trucks that match the non-address query
+    if (!isZipCode(query) && !isAddress(query) && address !== undefined) {
+        geoLocatedResults = await geoLocation(address);
+        filteredLocations = await mergeGeoResultsWithCsv(data, geoLocatedResults, defaultRadius);
+
+        filteredLocations = [...filteredLocations, ...filterByColumnValues(query, filteredLocations)];
+        return {geoLocatedResults, filteredLocations}
+    }
+
+    // case 3: address query with optional radius, and optional name
+    // geolocated address then filter through csv for lat / long points within radius distance of address
+    // and return filtered results that match name
+    if ((isZipCode(query) || isAddress(query)) && name !== undefined) {
+        geoLocatedResults = await geoLocation(query);
+        filteredLocations = mergeGeoResultsWithCsv(data, geoLocatedResults, defaultRadius);
+
+        filteredLocations = [...filteredLocations, ...filterByColumnValues(name, filteredLocations)];
+        return {geoLocatedResults, filteredLocations}
+    }
+
+    // case 2: non-address query
+    // filter through csv for non-address-value matches
+    if (!isZipCode(query) && !isAddress(query)) {
+        filteredLocations = filterByColumnValues(query, data);
+
+        return {geoLocatedResults, filteredLocations}
+    }
+
+    // case 1: address query with optional radius
+    // geolocate address then filter through csv for lat / long
+    // points within default radius distance of address
+    if ((isZipCode(query) || isAddress(query))) {
+        geoLocatedResults = await geoLocation(query);
+        filteredLocations = mergeGeoResultsWithCsv(data, geoLocatedResults, defaultRadius);
+
+        return {geoLocatedResults, filteredLocations}
+    }
+
+}
+
+module.exports = {
+    isZipCode,
+    isAddress,
+    precisionDistance,
+    searchFoodTrucks
+};
